c7695d09827b2d2809984ab9a64c57fdb6152c36
Puisque <36902836+Puisque@users.noreply.github.com>
2019-03-31 11:52:28 -0400

    Create struct.h
    struct for our instructions

---
+++ struct.h
@@ -0,0 +1,12 @@
+//
+// Created by Aisling Liu on 2019-03-31.
+//
+
+#ifndef ECE243_FINALPROJECT_STRUCT_H
+#define ECE243_FINALPROJECT_STRUCT_H
+typedef struct instructions{
+    string instruction;
+    //corresponding response to the instructions
+    int answer;
+}INST;
+#endif //ECE243_FINALPROJECT_STRUCT_H
