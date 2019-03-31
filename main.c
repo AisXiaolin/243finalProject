b925c9ad92d9f0c12badd37c60f1475cd9de380d
Puisque <36902836+Puisque@users.noreply.github.com>
2019-03-31 12:46:19 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,29 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "struct.h"
+#include <string.h>
 
 
-
-
-//load background png
-//
-
+//initialize instructions
+INST instruction[13] = {
+        {.instruction = "Not Left", .answer = 1},
+        {.instruction = "Not Right", .answer = 0},
+        {.instruction = "Not Not up", .answer = 2},
+        {.instruction = "Not Down", .answer = 2},
+        {.instruction = "Not Not Down", .answer = 3},
+        {.instruction = "Not Up", .answer = 3},//
+        {.instruction = "Not Not Left", .answer = 0},
+        {.instruction = "Not Not Right", .answer = 1},
+        {.instruction = "Not Not Not Left", .answer = 1},
+        {.instruction = "Not Not Not Right", .answer = 0},
+        {.instruction = "Not Not Not Up", .answer = 3},
+        {.instruction = "Not Not Not Down", .answer = 2},
+};
 
 
 
 
 int main() {
-
+    printf("Hello, World!\n");
+    return 0;
 }
