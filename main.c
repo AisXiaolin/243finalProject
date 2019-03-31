7e8a045eaf01b62304937c5da748f71e57525ed3
Puisque <36902836+Puisque@users.noreply.github.com>
2019-03-31 11:50:48 -0400

    main.c
    main program to display on the VGA

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "struct.h"
+
+
+
+
+//load background png
+//
+
+
+
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
